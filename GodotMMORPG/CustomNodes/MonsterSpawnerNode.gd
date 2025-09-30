abe931691a811d3b5e67d421c248672c5ea89c07
Neutrin0Dev <neutrinodevcontact@gmail.com>
2025-09-30 17:17:25 +0200

    push
    Refactor of the monster spawning code

--- GodotMMORPG/CustomNodes/MonsterSpawnerNode.gd
+++ GodotMMORPG/CustomNodes/MonsterSpawnerNode.gd
@@ -11,39 +11,37 @@ extends Area3D
 @export var MonsterPlaceHolder: Node3D # Node ou les monstres seront instancier
 # Variables internes
 var current_monsters_count: int = 0
-var players_in_zone: Array = []
-var spawn_timer: Timer
-var random_position
-
-
+var random_position : Vector3
 
 func _ready() -> void:
-	NetworkTime.after_tick_loop.connect(monster_spawning)
+	NetworkTime.on_tick.connect(monster_spawning)
 
-	
-func monster_spawning():
+func monster_spawning(delta, tick):
 	current_monsters_count = $MonsterPlaceHolder.get_child_count()
 	if current_monsters_count == 0 :
+		var position_calculated = random_position_calcule()
 		batch_monster_spawn()
-	
+
 func batch_monster_spawn():
+	for i in range(monster_counter):
+		var monster = monster_scene.instantiate() as CharacterBody3D
+		var calculated_position : Vector3 = random_position_calcule()
+		monster.name = name + str(i)
+		monster.set_multiplayer_authority(1)
+		MonsterPlaceHolder.add_child(monster)
+		monster.global_position = global_position + calculated_position
+
+func random_position_calcule():
 	var collision_shape : CollisionShape3D = $CollisionShape3D
 	var radius : float = 0.0
 	if collision_shape.shape is SphereShape3D:
 		var sphere_shape = collision_shape.shape as SphereShape3D
+		var position_calculated
 		radius = sphere_shape.radius
-
-	for i in range(monster_counter):
-		var monster = monster_scene.instantiate() as CharacterBody3D
 		random_position = Vector3(
 			rng.randf_range(-radius, radius),
 			0,
 			rng.randf_range(-radius, radius)
 		)
-		monster.name = name + str(i)
-		monster.set_multiplayer_authority(1)
-		MonsterPlaceHolder.add_child(monster)
-		monster.global_position = global_position + random_position
-		
-		
-	
+		position_calculated = random_position
+		return position_calculated
