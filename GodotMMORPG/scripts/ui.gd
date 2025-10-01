2dc529970432b382d8eca575131d297c068d010c
Neutrin0Dev <neutrinodevcontact@gmail.com>
2025-10-01 16:47:13 +0200

    push

--- GodotMMORPG/scripts/ui.gd
+++ GodotMMORPG/scripts/ui.gd
@@ -3,15 +3,16 @@ extends Control
 @onready var game_scene = preload("res://GodotMMORPG/scenes/game.tscn") 
 
 func _on_client_pressed() -> void:
-	get_tree().change_scene_to_packed(game_scene)
-	print("client change_scene")
+	#get_tree().change_scene_to_packed(game_scene)
+	#print("client change_scene")
 	NetworkHandler.start_client()
 	print("client started")
+	hide()
 	
 func _on_server_pressed() -> void:
-	get_tree().change_scene_to_packed(game_scene)
-	print("server change_scene")
+	#get_tree().change_scene_to_packed(game_scene)
+	#print("server change_scene")
 	NetworkHandler.start_server()
 	print("server started")
-	
+	hide()
 	
