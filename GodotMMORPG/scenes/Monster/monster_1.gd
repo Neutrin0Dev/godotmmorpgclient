57d43e72245034d1e3a1cbcd02a27e671345b9f9
Neutrin0Dev <neutrinodevcontact@gmail.com>
2025-10-01 17:45:57 +0200

    push

--- GodotMMORPG/scenes/Monster/monster_1.gd
+++ GodotMMORPG/scenes/Monster/monster_1.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @export var wander_radius: float 
 @export var wander_timer: float 
 @onready var rng = RandomNumberGenerator.new()
+
 var gravity = ProjectSettings.get_setting(&"physics/3d/default_gravity")
 var target_position: Vector3
 var time_until_next_wander: float = 0.0
@@ -16,24 +17,24 @@ func _ready() -> void:
 func _rollback_tick(delta, tick, is_fresh):
 	if is_multiplayer_authority():
 		prints(multiplayer.get_unique_id(), multiplayer.is_server(), global_position)
-	_force_update_is_on_floor()
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+		#_force_update_is_on_floor()
+		if not is_on_floor():
+			velocity.y -= gravity * delta
+			
+		time_until_next_wander -= delta
 		
-	time_until_next_wander -= delta
-	
-	if time_until_next_wander <= 0.0:		
-		var random_angle = rng.randf_range(0, TAU)
-		target_position = global_position + Vector3(cos(random_angle), 0, sin(random_angle)) * wander_radius
-		time_until_next_wander = wander_timer
+		if time_until_next_wander <= 0.0:		
+			var random_angle = rng.randf_range(0, TAU)
+			target_position = global_position + Vector3(cos(random_angle), 0, sin(random_angle)) * wander_radius
+			time_until_next_wander = wander_timer
+			
+		var direction = (target_position - global_position).normalized()
+		velocity.x = move_toward(velocity.x, direction.x, speed)
+		velocity.z = move_toward(velocity.z, direction.z, speed)
 		
-	var direction = (target_position - global_position).normalized()
-	velocity.x = move_toward(velocity.x, direction.x, speed)
-	velocity.z = move_toward(velocity.z, direction.z, speed)
-	
-	velocity *= NetworkTime.physics_factor
-	move_and_slide()
-	velocity /= NetworkTime.physics_factor
+		velocity *= NetworkTime.physics_factor
+		move_and_slide()
+		velocity /= NetworkTime.physics_factor
 
 func _force_update_is_on_floor():
 	var old_velocity = velocity
