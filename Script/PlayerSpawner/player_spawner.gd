4f85b9e71235c0ddc297a84d17477b561e57420f
Neutrin0Dev <neutrinodevcontact@gmail.com>
2025-10-08 17:31:35 +0200

    push

--- Script/PlayerSpawner/player_spawner.gd
+++ Script/PlayerSpawner/player_spawner.gd
@@ -4,22 +4,19 @@ extends Node3D
 @export var player_scene: PackedScene
 
 func _ready() -> void:
-	await get_tree().process_frame
+	
 	# ⭐ SERVEUR DÉDIÉ : Pas de joueur pour le host
 	if not multiplayer.is_server():
 		print("PlayerSpawner ready on client side")
 		print("Call the spawning RPC to the server")
 		var player_id = multiplayer.get_unique_id()
-		player_is_ready(player_id)
+		spawn_player.rpc(player_id)
 		return
-
-func player_is_ready(player_id):
-	if NetworkHandler.player_list.has(player_id):
-		print("Le ", player_id, " est présent dans la playerlist")
-		spawn_player.rpc_id(1,player_id)
 	
 @rpc("any_peer","call_remote","reliable")
 func spawn_player(player_id):
 	var player = player_scene.instantiate()
 	player.name = str(player_id)
 	add_child(player, true)
+	
+	set_multiplayer_authority(1)
